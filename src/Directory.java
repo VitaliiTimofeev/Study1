480def6c0cddfe647a5112d71c8951c9585e5889
VitaliiTimofeev <vital9xd@gmail.com>
2022-02-05 18:12:44 +0300

    Traning

--- src/Directory.java
+++ src/Directory.java
@@ -1,5 +1,11 @@
+import java.util.ArrayList;
+
 public class Directory {
 
+    ArrayList <File> list = new ArrayList<>();
 
+    public void addFiles(File file) {
+
+    }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,11 @@ public class Main {
     public static void main(String[] args) {
 
        Directory dir1 = new Directory();
-       dir1.addFiles(new File("Привет", ".txt"...));
+       dir1.addFiles(new File("txt","Скрытый", Atributs.READ, 12,true,false ));
        System.out.println(dir1);
-       dir1.addFiles(new File("Привет", ".txt"...));
-       dir1.addFiles(new File("Привет", ".txt"...));
+       /* Directory dir2 = new Directory();
+       dir1.addFiles(new File());
+       System.out.println(dir2);  */
+
     }
 }
